a2de138d1cba50aa1e30f13e2c952cf1440063cf
hung-le <none>
2018-03-12 18:32:25 -0700

    Allow user to edit the list of currency.

--- hleOfxQuotes-gui/src/main/java/com/hungle/msmoney/gui/action/EditCurrencyAction.java
+++ hleOfxQuotes-gui/src/main/java/com/hungle/msmoney/gui/action/EditCurrencyAction.java
@@ -8,7 +8,10 @@ import java.util.TreeSet;
 
 import javax.swing.AbstractAction;
 import javax.swing.Icon;
+import javax.swing.JComboBox;
 import javax.swing.JOptionPane;
+
+import org.apache.log4j.Logger;
 
 import com.hungle.msmoney.core.ofx.CurrencyUtils;
 import com.hungle.msmoney.gui.GUI;
@@ -17,11 +20,13 @@ import com.hungle.msmoney.gui.GUI;
  * The Class EditCurrencyAction.
  */
 public final class EditCurrencyAction extends AbstractAction {
+    private static final Logger LOGGER = Logger.getLogger(EditCurrencyAction.class);
 
     /**
      * 
      */
     private final GUI gui;
+
     /** The Constant serialVersionUID. */
     private static final long serialVersionUID = 1L;
 
@@ -46,42 +51,63 @@ public final class EditCurrencyAction extends AbstractAction {
      */
     @Override
     public void actionPerformed(ActionEvent e) {
-        Set<String> keys = CurrencyUtils.CURRENCIES.keySet();
-        keys = addCurrentJavaLocaleCurrency(keys);
+        Set<String> labels = CurrencyUtils.CURRENCIES.keySet();
+        labels = addCurrentJavaLocaleCurrency(labels);
 
-        String[] possibilities = new String[keys.size()];
+        String[] possibilities = new String[labels.size()];
         int i = 0;
-        for (String key : keys) {
+        for (String key : labels) {
             possibilities[i++] = key;
         }
+
         Icon icon = null;
-        String s = (String) JOptionPane.showInputDialog(this.gui, "Current: " + this.gui.getDefaultCurrency() + "\n" + "Available:",
-                "Set Currency", JOptionPane.PLAIN_MESSAGE, icon, possibilities, null);
+        String title = "Currency: current " + this.gui.getDefaultCurrency();
+
+        JComboBox<String> jcb = new JComboBox<String>(possibilities);
+        jcb.setEditable(true);
+        int confirm = JOptionPane.showConfirmDialog(this.gui, jcb, title, JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE,
+                icon);
+        if (confirm == JOptionPane.CANCEL_OPTION) {
+            return;
+        }
+
+        String selectedLabel = (String) jcb.getSelectedItem();
 
         // If a string was returned, say so.
-        if ((s != null) && (s.length() > 0)) {
-            String value = CurrencyUtils.CURRENCIES.get(s);
-            if ((value == null) || (value.length() <= 0)) {
-                value = s;
+        if ((selectedLabel != null) && (selectedLabel.length() > 0)) {
+            String currencyCode = null;
+            currencyCode = CurrencyUtils.CURRENCIES.get(selectedLabel);
+            if ((currencyCode == null) || (currencyCode.length() <= 0)) {
+                currencyCode = selectedLabel;
             }
-            this.gui.selectNewCurrency(value);
+            
+            if (currencyCode != null) {
+                this.gui.selectNewCurrency(currencyCode);
+            }
         } else {
         }
     }
 
-    private Set<String> addCurrentJavaLocaleCurrency(Set<String> currentSet) {
+    private Set<String> addCurrentJavaLocaleCurrency(Set<String> labels) {
         Locale locale = Locale.getDefault();
         Currency currency = Currency.getInstance(locale);
-        String currencyCode = currency.getCurrencyCode();
+        String defaultCurrencyCode = currency.getCurrencyCode();
 
-        if (currentSet.contains(currencyCode)) {
-            return currentSet;
+        for (String label : labels) {
+            String currencyCode = CurrencyUtils.CURRENCIES.get(label);
+            if (currencyCode != null) {
+                if (currencyCode.compareToIgnoreCase(defaultCurrencyCode) == 0) {
+                    return labels;
+                }
+            }
+
         }
 
-        Set<String> set = new TreeSet<>();
-        set.addAll(currentSet);
-        set.add(currencyCode);
+        Set<String> newLabels = new TreeSet<>();
+        newLabels.addAll(labels);
+        newLabels.add(defaultCurrencyCode);
+        LOGGER.info("Added new currencyCode=" + defaultCurrencyCode);
 
-        return set;
+        return newLabels;
     }
 }
